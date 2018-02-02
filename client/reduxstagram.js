a70ff8164c20d2f1b431907c56c53a1c132768bd
CalvinStar <vladislavgriga@gmail.com>
2018-02-02 17:15:12 +0300

    Set up dependecies and styles for future project

---
+++ client/reduxstagram.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+/* Import CSS */
+import css from './styles/style.styl';
+
+render( < h1 > VLadislav Best < /h1>, document.getElementById('root'));
